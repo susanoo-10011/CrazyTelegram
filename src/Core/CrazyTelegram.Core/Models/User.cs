969ddbf18c158e2d2a1cc75f24c2de08b89d5d64
Egor <cycahosir@gmail.com>
2024-10-08 10:11:30 +0700

    Добавил новые папки для  разных слоев

---
+++ src/Core/CrazyTelegram.Core/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyTelegram.Core.Models
+{
+    public class User
+    {
+        private User(Guid id, string userName, string passwordHash, string email)
+        {
+            Id = id;
+            UserName = userName;
+            PasswordHash = passwordHash;
+            Email = email;
+        }
+
+        public Guid Id { get; set; }
+
+        public string UserName { get; private set; }
+
+        public string PasswordHash { get; private set; }
+
+        public string Email { get; private set; }
+
+
+        public static User Create(Guid id, string userName, string passwordHash, string email)//создание юзера
+        {
+            return new User(id, userName, passwordHash, email);
+        }
+
+    }
+}
